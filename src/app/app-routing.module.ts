eeff6edab52bfa39c874946196b670f12666d019
Stargazer <chris@antisphex.net>
2019-04-23 20:54:42 +0200

    work on navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShoppinglistComponent} from './components/shoppinglist/shoppinglist.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'shoppingList', component: ShoppinglistComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
